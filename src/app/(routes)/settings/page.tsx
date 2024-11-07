b4e0b32f97215e0cbffc6f497b7715ea4fadd2e7
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-11-07 21:05:43 +0530

    update details ui done

--- src/app/(routes)/settings/page.tsx
+++ src/app/(routes)/settings/page.tsx
@@ -1,7 +1,37 @@
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { CloudUploadIcon } from "lucide-react";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const SettingsPage = () => {
+  return (
+    <div className="max-w-lg mx-auto">
+      <h1 className="text-2xl font-bold mb-4 text-center">Profile Settings</h1>
+      <form action="">
+        <div className="flex gap-8 items-center">
+          <div className="bg-gray-500 size-24 rounded-full" />
+          <div>
+            <Button variant="surface" size={"4"}>
+              <CloudUploadIcon />
+              change avatar
+            </Button>
+          </div>
+        </div>
+        <p className="mt-2 font-bold">username:</p>
+        <TextField.Root placeholder="your_username" />
+        <p className="mt-2 font-bold">name:</p>
+        <TextField.Root placeholder="Jon Deo" />
+        <p className="mt-2 font-bold">subtitle:</p>
+        <TextField.Root placeholder="Web devloper" />
+        <p className="mt-2 font-bold">bio:</p>
+        <TextArea />
+        <div className="mt-2 flex justify-center">
+          <Button variant="solid" size={"3"}>
+            Update Details
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
-export default page;
+export default SettingsPage;
