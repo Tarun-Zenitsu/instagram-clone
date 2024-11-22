72f1d11e40d7ab898d641ce287e5c20be68abedf
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-11-22 22:18:51 +0530

    Home page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,18 +1,14 @@
 import SignIn from "../../components/sign-in";
 import { auth } from "@/auth";
 import SignOut from "../../components/sign-out";
+import HomeTopRow from "@/components/HomeTopRow";
+import UserHome from "@/components/UserHome";
 
 export default async function Home() {
   const session = await auth();
   return (
     <main className="">
-      hi there
-      <br />
-      {session && (
-        <div className="flex items-center justify-center cursor-pointer">
-          <SignOut />
-        </div>
-      )}
+      {session && <UserHome session={session} />}
       {!session && (
         <div className="flex items-center justify-center cursor-pointer">
           <SignIn />
---
+++ src/components/UserHome.tsx
@@ -0,0 +1,23 @@
+import HomeTopRow from "@/components/HomeTopRow";
+import { Session } from "next-auth";
+import { prisma } from "./db";
+import HomePosts from "./HomePosts";
+
+export default async function UserHome({ session }: { session: Session }) {
+  const follows = await prisma.follower.findMany({
+    where: {
+      followingProfileEmail: session?.user?.email || "",
+    },
+  });
+  const profiles = await prisma.profile.findMany({
+    where: {
+      id: { in: follows.map((f) => f.followedProfileId) },
+    },
+  });
+  return (
+    <div className="flex flex-col gap-8">
+      <HomeTopRow follows={follows} profiles={profiles} />
+      <HomePosts follows={follows} profiles={profiles} />
+    </div>
+  );
+}
